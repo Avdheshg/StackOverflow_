e05f620ee8df7700cffaa74b420dd3c9d67a0153
Avduesh Gautam <agt@leapwork.com>
2025-04-01 13:43:10 +0530

    Add project files.

---
+++ ICommentable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    public interface ICommentable
+    {
+        void AddComment(Comment comment);
+        List<Comment> GetComments();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace StackOverflow
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            StackOverflowDemo.Run();
+        }
+    }
+}
